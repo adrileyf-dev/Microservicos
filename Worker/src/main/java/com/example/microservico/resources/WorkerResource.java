12ac5154f1bbfad0e5e36a69ac81737649b74232
adriley <adrileyfdev@gmail.com>
2024-07-17 21:46:14 -0300

    server config

--- Worker/src/main/java/com/example/microservico/resources/WorkerResource.java
+++ Worker/src/main/java/com/example/microservico/resources/WorkerResource.java
@@ -3,11 +3,9 @@ package com.example.microservico.resources;
 import com.example.microservico.entities.Worker;
 import com.example.microservico.repositories.WorkRepository;
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
-
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,24 +13,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.logging.Logger;
 
 @RestController
-@AllArgsConstructor
+
 @Log4j2
 
 @RequestMapping(value = "/workers")
 
 public class WorkerResource {
+    @Value("${worker.config}")
+    private String testConfig;
 
-    private  final WorkRepository workRepository;
+     @Autowired
+     WorkRepository workRepository;
 
-    private Environment env;
 
     @GetMapping
     public ResponseEntity<List<Worker>> findAll() {
         List<Worker> workerList = workRepository.findAll();
         return ResponseEntity.ok(workerList);
+    }
+
+    @GetMapping(value = "configs")
+    public ResponseEntity<Void> getConfigs() {
+         log.info("CONFIG " + testConfig);
+
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping(value = "/{id}")
